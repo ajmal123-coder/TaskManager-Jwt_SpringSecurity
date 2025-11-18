c1ed02b7c7d46979926e727d695a5d60307c07bc
Ajmal <ajmalsadhath@gmail.com>
2025-11-18 21:18:44 +0530

    dto files created

---
+++ src/main/java/com/Task/Manager/dto/AuthRequest.java
@@ -0,0 +1,8 @@
+package com.Task.Manager.dto;
+
+import lombok.Data;
+
+@Data public class AuthRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/Task/Manager/dto/TaskRequest.java
@@ -0,0 +1,10 @@
+package com.Task.Manager.dto;
+
+import lombok.Data;
+
+@Data
+public class TaskRequest {
+
+    private String title;
+    private String description;
+}
